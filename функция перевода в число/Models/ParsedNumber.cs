79d0bdaec1be47475347f7045ee5f582a55791e2
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-17 14:38:57 +1000

    класс ParsedNumber для результатов конвертации

--- функция перевода в число/Models/ParsedNumber.cs
+++ функция перевода в число/Models/ParsedNumber.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace WordToNumber.Models
 {
-    internal class ParsedNumber
+        //разбор числительных
+    public class ParsedNumber
     {
+        // значение
+        public double NumerValue { get; set; }
+        // оригинальный текст
+        public string OriginalText { get; set; }
+        // ошибки?
+        public bool HasErrors { get; set; }
+        //какие ошибки?
+        public List<string> ErrorMessages { get; set; }
+        //отрицательное число?
+        public bool ISNegative { get; set; }
+        // дробное число?
+        public bool HasDrobPart { get; set; }
+        // конструкторв
+        public ParsedNumber() 
+        { 
+            ErrorMessages = new List<string>();
+        }
+
+        public ParsedNumber(double value, string text)
+        {
+            NumerValue = value;
+            OriginalText = text;
+            ErrorMessages = new List<string>();
+        }
     }
 }
