c9c80dba3ad18423f7d3001308761951aa96cb03
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-17 14:42:26 +1000

    интерфейс для конвертера

--- функция перевода в число/Interfaces/INumberConverter.cs
+++ функция перевода в число/Interfaces/INumberConverter.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace WordToNumber.Interfaces
 {
-    internal class INumberConverter
+    //интерфейс для конвертера слов в числа 
+    public interface INumberConverter
     {
+        // метод конвертации
+        Models.ParsedNumber ConvertWordsToNumber(string words);
+        // валидации
+        bool valid(string words, out List<string> errors);
     }
 }
