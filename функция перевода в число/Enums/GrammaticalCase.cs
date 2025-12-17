f8f06a344483d16691e801d1e34cb35060daf23f
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-17 12:02:41 +1000

    падежи русского языка

--- функция перевода в число/Enums/GrammaticalCase.cs
+++ функция перевода в число/Enums/GrammaticalCase.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace WordToNumber.Enums
 {
-    internal class GrammaticalCase
+    public enum GrammaticalCase
     {
+        Nominative, // именительный один два
+
+        Genitive, // родительный одного двух
+
+        Dative, // дательный одному двум
+
+        Accusative, // винительный один два(кого что)
+
+        Instrumental, // творительный одним двумя
+
+        Prepositional // предложный об одном о двух
     }
 }
