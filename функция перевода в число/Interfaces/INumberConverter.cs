610d96f335dbd1bac15793b41facaa315e2dd68b
kiryshaboshki <danilovkirill58@gmail.com>
2025-12-17 11:55:51 +1000

    Добавьте файлы проекта.

---
+++ функция перевода в число/Interfaces/INumberConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordToNumber.Interfaces
+{
+    internal class INumberConverter
+    {
+    }
+}
